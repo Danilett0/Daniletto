4d5b4e7b8b0d69a7daa3b445023641c8640daf0d
Danilett0 <daniilo.97@hotmail.com>
2023-06-18 23:32:34 -0500

    completo pagina de portafolio

--- src/Pages/Portafolio.jsx
+++ src/Pages/Portafolio.jsx
@@ -4,22 +4,21 @@ import { VisualEffectTopWhite } from "../Components/ShapeVisualEffects";
 import "../App.css";
 import "../styles/pages/Portafolio.css";
 
-function Portafolio(props) {
+function Portafolio() {
   return (
     <div className="Portafolio">
       <div className="Encabezado">
         <div className="Texto">
           <h1>PORTAFOLIO DE DISEÑO WEB</h1>
           <p>
-            En Daniletto, nos enfocamos en tres cosas: calidad, personalización
-            y atención al cliente. Nos aseguramos de que cada sitio web sea
-            único y refleje la marca y los valores de nuestros clientes. Además,
-            ofrecemos un servicio integral que incluye diseño, desarrollo,
-            mantenimiento y soporte técnico. </p>
-            <p> Nuestro objetivo es crear sitios
-            web funcionales, atractivos y optimizados para los motores de
-            búsqueda. Queremos que nuestros clientes se sientan satisfechos y
-            orgullosos de su presencia en línea.
+            En Daniletto, me enfoco en tres cosas: calidad, personalización y
+            atención al cliente. Me aseguro de que cada sitio web sea único y
+            refleje la marca y los valores de mis clientes. Además, ofrezco un
+            servicio integral que incluye diseño, desarrollo, mantenimiento y
+            soporte técnico. <br />
+            <br /> Mi objetivo es crear sitios web funcionales, atractivos y
+            optimizados para los motores de búsqueda. Quiero que mis clientes se
+            sientan satisfechos y orgullosos de su presencia en línea.
           </p>
         </div>
         <img src="/images/landing.png" alt="imagen landing page" />
@@ -28,41 +27,55 @@ function Portafolio(props) {
       <VisualEffectTopWhite />
 
       <div className="Proyectos">
-        <h2>TE PRESENTO ALGUNOS <br/> DE MIS PROYECTOS FAVORITOS</h2>
+        <h2>
+          Te presento algunos <br /> de mis proyectos favoritos
+        </h2>
 
         <div className="Box-Proyectos">
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/fyllo.jpeg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Landing-Page-Fyllo/">
+              VER SITIO WEB
+            </Link>
           </div>
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/BookMark.jpeg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Landing-Page-Bokmark/">
+              VER SITIO WEB
+            </Link>
           </div>
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/FincaRaiz.jpeg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Pagina-Web-Finca-Raiz/">
+              VER SITIO WEB
+            </Link>
           </div>
         </div>
 
         <div className="Box-Proyectos">
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/LifeMiles.jpg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Landing-Page-Lifemiles/">
+              VER SITIO WEB
+            </Link>
           </div>
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/Shortly.jpeg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Landing-Page-Shortli/">
+              VER SITIO WEB
+            </Link>
           </div>
           <div className="Proyecto">
-            <img src="/images/proyecto-fyllo.jpg" alt="imagen de proyecto" />
+            <img src="/images/ClaudiaBotero.jpeg" alt="imagen de proyecto" />
             <h4>Landing Page Fyllo</h4>
-            <Link to="">VER SITIO WEB</Link>
+            <Link to="https://danilett0.github.io/Landing-Page-Psicologa/">
+              VER SITIO WEB
+            </Link>
           </div>
         </div>
       </div>
