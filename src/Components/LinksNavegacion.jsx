f2d88b78315ff4aabafe4ec0be93d8b9a86972e9
Danilett0 <daniilo.97@hotmail.com>
2023-06-21 18:07:08 -0500

    correccion

--- src/Components/LinksNavegacion.jsx
+++ src/Components/LinksNavegacion.jsx
@@ -2,34 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/components/LinksNavegacion.css";
 
-const Home = React.lazy(() => import("../Pages/Home"));
-const About = React.lazy(() => import("../Pages/About"));
-const Contacto = React.lazy(() => import("../Pages/Contacto"));
-const Portafolio = React.lazy(() => import("../Pages/Portafolio"));
-
 function LinksNavegacion(props) {
   return (
     <nav aria-label="links de navegacion">
       <ul>
         <li>
-          <NavLink to={<Home />} activeclassname="active">
+          <NavLink to="/Daniletto" activeclassname="active">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to={<Portafolio />} activeclassname="active">
+          <NavLink to="/Portafolio" activeclassname="active">
             Portafolio
           </NavLink>
         </li>
         <li>
-          <NavLink to={<About />} activeclassname="active">
+          <NavLink to="/About" activeclassname="active">
             Sobre mí
           </NavLink>
         </li>
         <li>
           <NavLink
             className="LinkContacto"
-            to={<Contacto />}
+            to="/Contacto"
             activeclassname="active"
           >
             Contacto
