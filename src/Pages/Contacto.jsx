623b8287c10727a663eb34223634c95a5216978e
Danilett0 <daniilo.97@hotmail.com>
2023-07-23 18:44:06 -0500

    aumento tiempo de carga para contacto

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -10,7 +10,7 @@ import "../App.css";
 import "../styles/pages/Contacto.css";
 
 function Contacto() {
-  const loadingPage = useLoading("contacto", 300);
+  const loadingPage = useLoading("contacto", 500);
   return (
     <>
       {loadingPage ? (
