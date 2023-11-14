4eeda6ca45dad6195eb115f3593cfd58f37aa49d
Danilett0 <daniilo.97@hotmail.com>
2023-11-14 00:39:11 -0500

    implementacion hfc mejora visual

--- src/Components/SoportCos/HFC.jsx
+++ src/Components/SoportCos/HFC.jsx
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
 import RazonLlamada from "./RazonLlamada";
+import Tipologia from "./Tipologia";
 
 function HFC(props) {
 
   const [razonLlamada, SetRazonLlamada] = useState(false)
   const [perfilEscala, SetPerfilEscala] = useState(false)
-
+  const [tipologia, SetTipologia] = useState(false)
 
   return (
     <div className="MainHfc">
-      <button onClick={() => {
-        SetRazonLlamada(true)
-        SetPerfilEscala(false)
-      }}>Razon de llamada</button>
 
-      <button onClick={() => {
-        SetPerfilEscala(true)
-        SetRazonLlamada(false)
-      }}>
-        Perfiles Escalamiento
-      </button>
+      <div className="BtnOpciones">
+        <button className="Button Red" onClick={() => {
+          SetRazonLlamada(true)
+          SetPerfilEscala(false)
+          SetTipologia(false)
+        }}>Razon de llamada</button>
+
+        <button className="Button Blue" onClick={() => {
+          SetTipologia(true)
+          SetRazonLlamada(false)
+          SetPerfilEscala(false)
+        }}>Tipologia de llamada</button>
+
+        <button className="Button Green" onClick={() => {
+          SetPerfilEscala(true)
+          SetRazonLlamada(false)
+          SetTipologia(false)
+        }}>
+          Perfiles Escalamiento
+        </button>
+      </div>
 
       {razonLlamada && (
         <RazonLlamada />
       )}
 
       {perfilEscala && (
-        <h4>|SOPNROCCID|SERVIDES1|</h4>
+        <div className="MainRazonLlamada">
+          <div className="Box-Razones">
+            <div className="Bloque">
+              <h3>Internet</h3>
+              <div> <span>1 </span> SOPNROCCID</div>
+              <div> <span>2 </span> SERVIDES1</div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {tipologia && (
+        <Tipologia />
       )}
 
     </div>
